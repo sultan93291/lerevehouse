cf0fc5a7eb7e2d5337319446d78836d521e30597
sultan93291 <abibdipto@gmail.com>
2025-03-01 17:39:59 +0600

    destination details page done

--- src/components/TourListDetails/TourListDetailsTab.jsx
+++ src/components/TourListDetails/TourListDetailsTab.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-scroll';
 const TourListDetailsTab = ({ sectionTabs }) => {
   const [activeTab, setActiveTab] = useState(sectionTabs[0]);
   return (
-    <div className="py-4 2xl:sticky z-[600] top-[152px] left-0 font-interTight w-full bg-[#f4f4f4] flex flex-row flex-wrap items-center lg:justify-center gap-3 2xl:gap-12 px-4 lg:px-8 2xl:px-16 3xl:px-32 ">
+    <div className="py-4 2xl:sticky z-[600] top-[152px] left-0 font-interTight w-full bg-[#f4f4f4] flex flex-row flex-wrap items-center lg:justify-center gap-3 3xl:gap-12 px-4 lg:px-8 2xl:px-16 3xl:px-32 ">
       <div className="font-editorsNoteItalic text-primary text-3xl lg:text-3xl border-b-[3px] pb-2 px-4 lg:px-8 border-[#f4f4f4]">
         Trip Details
       </div>
