a842b842a261ddc7bb668c86b2ba76a10534a128
tofiqsoftvance <tofiqulislam.softvance@gmail.com>
2025-03-02 15:40:43 +0600

    Tour Guide done

--- src/components/TourGuide/FestivalsCalendar.jsx
+++ src/components/TourGuide/FestivalsCalendar.jsx
@@ -1,7 +1,39 @@
 const FestivalsCalendar = () => {
     return (
-        <div>
-            FestivalsCalendar
+        <div className="space-y-10">
+            <div className="pb-6 border-b border-gray-200">
+                <h3 className="font-fontSpring text-3xl mb-2 text-[#004265]">Dragon boat Festival</h3>
+                <h5 className="font-fontSpring text-xl mb-2 text-[#004265]">Dates: late June</h5>
+                <p className="text-[#565656] text-lg">Maenam is one of Vancouvers best Thai restaurants, offering refined, beautifully-presented, authentic cuisine prepared using the best seasonal and local ingredients. The menu includes familiar classics like the sweet and sour tom kaa soup, curries and pad Thai. Wine, beer and cocktail menu is also avalable.
+                </p>
+            </div>
+            <div className="pb-6 border-b border-gray-200">
+                <h3 className="font-fontSpring text-3xl mb-3 text-[#004265]">Festival d’été francophone </h3>
+                <h5 className="font-fontSpring text-xl mb-2 text-[#004265]">Date: mid June</h5>
+                <p className="text-[#565656] text-lg">Maenam is one of Vancouvers best Thai restaurants, offering refined, beautifully-presented, authentic cuisine prepared using the best seasonal and local ingredients. The menu includes familiar classics like the sweet and sour tom kaa soup, curries and pad Thai. Wine, beer and cocktail menu is also avalable.
+                </p>
+            </div>
+            <div className="pb-6 border-b border-gray-200">
+                <h3 className="font-fontSpring text-3xl mb-3 text-[#004265]">Vancouver International Jazz Festival</h3>
+                <h5 className="font-fontSpring text-xl mb-2 text-[#004265]">Dates: late June</h5>
+                <p className="text-[#565656] text-lg">This cozy, modern Chinese brasserie is a far cry from the Chinese restaurants of old. You will be charmed by the fresh ingredients, friendly staff, trendy atmosphere and the impeccable presentation. In a city that has long embraced Chinese cuisine as part of its culinary heritage, Bao Bei exemplifies the changing face of Chinatown.
+                </p>
+            </div>
+            <div className="pb-6 border-b border-gray-200">
+                <h3 className="font-fontSpring text-3xl mb-3 text-[#004265]">L’Vancouver Folk Festival</h3>
+                <h5 className="font-fontSpring text-xl mb-2 text-[#004265]">Dates: mid July</h5>
+                <p className="text-[#565656] text-lg">This annual three-day festival features folk music from around the globe, performed in a beautiful outdoor venue. One of Canada&apos;s most popular folk music festivals, it attracts some 30,000 folk music fans every year and features 30 to 40 concerts on eight outdoor stages.</p>
+            </div>
+            <div className="pb-6 border-b border-gray-200">
+                <h3 className="font-fontSpring text-3xl mb-3 text-[#004265]">Celebration of Light Fireworks Festival</h3>
+                <h5 className="font-fontSpring text-xl mb-2 text-[#004265]">Dates: end of July</h5>
+                <p className="text-[#565656] text-lg">Celebration of Light is an international musical fireworks competition. Competing countries light up the sky with spectacular fireworks, carefully synchronized to music. Whether you watch it from the shore or from a boat out on the water, this festival is a delight for all ages.</p>
+            </div>
+            <div className="pb-6 border-gray-200">
+                <h3 className="font-fontSpring text-3xl mb-3 text-[#004265]">Pride Vancouver</h3>
+                <h5 className="font-fontSpring text-xl mb-2 text-[#004265]">Date: early August Pride Parade</h5>
+                <p className="text-[#565656] text-lg">Like Montreal, Toronto and many other big cities across the globe, Vancouver celebrates diversity. A week of festivities culminates in a flamboyant parade stretching along two kilometres of city streets and attracting close to half a million spectators and participants..</p>
+            </div>
         </div>
     );
 };
--- src/components/TourGuide/WhereToStay.jsx
+++ src/components/TourGuide/WhereToStay.jsx
@@ -1,7 +1,20 @@
+import slide from "../../assets/images/tour-guide/slide.png"
+
 const WhereToStay = () => {
     return (
         <div>
-            WhereToStay
+            <h3 className="text-2xl font-medium mb-3 text-[#004265]">Palazzo Petrvs</h3>
+            <p className="text-[#565656] text-lg">
+                Nestled within a historic former palace in the heart of Umbria&apos;s Orvieto,
+                Palazzo Petrvs is steps from the Duomo Orvieto. The luxury boutique hotel has nine
+                stately guest rooms and suites with original architectural elements, bold textiles,
+                and contemporary furnishings. A restaurant and bar specialize in Italian cuisine.
+                Catering to guests in search of history and luxury, Palazzo Petrvs offers a central
+                location for exploring Orvieto’s Etruscan roots.
+            </p>
+            <figure className="rounded mt-5">
+                <img src={slide} alt="w-full h-full object-cover rounded" />
+            </figure>
         </div>
     );
 };
