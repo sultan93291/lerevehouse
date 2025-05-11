4612efea32a1a0ced8ee1718d23e053fbe790572
sultan93291 <abibdipto@gmail.com>
2025-05-11 14:33:26 +0600

    feat:transportation honeymoon api integration done

--- src/Pages/ViagaNozi/ViagaNozi.jsx
+++ src/Pages/ViagaNozi/ViagaNozi.jsx
@@ -3,18 +3,36 @@ import React from "react";
 import ExploreJourney from "@/components/TravelStylesDetails/ExploreJourney";
 import FeaturedTravels from "@/components/TravelStylesDetails/FeaturedTravels";
 import HoneyMoon from "../../assets/images/honey-moon/honeymoon.png";
-import { useGetTripPackageDetailsQuery } from "@/Redux/features/api/apiSlice";
+import {
+  useGetHoneyMoonHeroSectionDataQuery,
+  useGetTransportationHeroSectionDataQuery,
+  useGetTripPackageDetailsQuery,
+} from "@/Redux/features/api/apiSlice";
 import { InfinitySpin } from "react-loader-spinner";
 import toast from "react-hot-toast";
 
 const ViagaNozi = () => {
-  const  id  = 1
+  const id = 1;
   const { data, error, isLoading } = useGetTripPackageDetailsQuery(id, {
     refetchOnFocus: true,
     refetchOnReconnect: true,
   });
 
-  if (isLoading) {
+    const { data:transportData, error:transportError, isLoading:isTransportLoading } = useGetTransportationHeroSectionDataQuery(id, {
+      refetchOnFocus: true,
+      refetchOnReconnect: true,
+    });
+
+  const {
+    data: honeyMoondata,
+    error: honeyMoonError,
+    isLoading: isHoneymoonLoading,
+  } = useGetHoneyMoonHeroSectionDataQuery(id, {
+    refetchOnFocus: true,
+    refetchOnReconnect: true,
+  });
+
+  if (isLoading || isHoneymoonLoading) {
     return (
       <div className="fixed top-0 left-0 w-screen h-screen flex items-center justify-center z-50 bg-white">
         <InfinitySpin
@@ -27,11 +45,18 @@ const ViagaNozi = () => {
     );
   }
 
-  if (error) {
+  if (error || honeyMoonError) {
     const errorMessage =
       error.data?.message || error.error || error.status || error.message;
+    const honeyMoonErrorMsg =
+      honeyMoonError.data?.message ||
+      honeyMoonError.error ||
+      honeyMoonError.status ||
+      honeyMoonError.message;
     if (errorMessage) {
       toast.error(errorMessage);
+    } else if (honeyMoonError) {
+      toast.error(honeyMoonErrorMsg);
     }
   }
 
@@ -40,8 +65,8 @@ const ViagaNozi = () => {
   return (
     <div>
       <CommonHeroBanner
-        bg={`${imgBaseurl}/${data?.data[0].travel_style?.image}`}
-        title="VIAGGI NOZZE"
+        bg={`${imgBaseurl}/${honeyMoondata?.data?.background_image}`}
+        title={honeyMoondata?.data?.title}
       />
       {/* <ExploreJourney isBtn={false} btnTxt={"All honeymoon Trips "} /> */}
       <FeaturedTravels data={data?.data} isHoneyMoon={true} />
