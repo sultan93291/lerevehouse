6829c51c618dde04bd27c1340bd77df2d98dcf64
fahim-sarker <sarkerfahim599@gmail.com>
2025-07-15 10:22:04 +0600

    initialize i18n

--- src/i18n.js
+++ src/i18n.js
@@ -1,19 +1,30 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 
-import en from "./locales/en.json";
-import it from "./locales/it.json";
+import translationEN from "./locales/en.json";
+import translationIT from "./locales/it.json";
 
-i18n.use(initReactI18next).init({
-  resources: {
-    en: { translation: en },
-    it: { translation: it },
+const resources = {
+  en: {
+    translation: translationEN,
   },
-  lng: localStorage.getItem("lan") || "en", 
-  fallbackLng: "en",
-  interpolation: {
-    escapeValue: false,
+  it: {
+    translation: translationIT,
   },
-});
+};
+
+i18n
+  .use(LanguageDetector) // optional: auto-detect user language
+  .use(initReactI18next)
+  .init({
+    resources,
+    fallbackLng: "en", // fallback language
+    debug: true, // logs language events in console
+
+    interpolation: {
+      escapeValue: false, // react already protects from xss
+    },
+  });
 
 export default i18n;
