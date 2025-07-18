483223e005ece33cb03e1a5acbb7fdc0a5ad2fd3
fahim-sarker <sarkerfahim599@gmail.com>
2025-07-18 17:02:53 +0600

    fixed the i18 issues on languageslice

--- src/Redux/features/languageSlice.js
+++ src/Redux/features/languageSlice.js
@@ -1,5 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
-import i18n from "@/i18n"; 
+import i18n from "@/i18n";
 
 const initialState = {
   language: localStorage.getItem("lan") || "en",
@@ -9,9 +9,7 @@ const languageSlice = createSlice({
   name: "language",
   initialState,
 
-  
   reducers: {
-
     setLanguage: state => {
       if (!state.language) {
         state.language = "en";
@@ -20,11 +18,48 @@ const languageSlice = createSlice({
       } else if (state.language === "it") {
         state.language = "en";
       }
+
+      i18n.changeLanguage(state.language);
+      localStorage.setItem("lan", state.language);
+      localStorage.setItem("i18nextLng", state.language); 
+
       window.location.reload();
-   
-    }
+    },
   },
 });
 
 export const { setLanguage } = languageSlice.actions;
 export default languageSlice.reducer;
+
+
+
+// import { createSlice } from "@reduxjs/toolkit";
+// import i18n from "@/i18n"; 
+
+// const initialState = {
+//   language: localStorage.getItem("lan") || "en",
+// };
+
+// const languageSlice = createSlice({
+//   name: "language",
+//   initialState,
+
+  
+//   reducers: {
+
+//     setLanguage: state => {
+//       if (!state.language) {
+//         state.language = "en";
+//       } else if (state.language === "en") {
+//         state.language = "it";
+//       } else if (state.language === "it") {
+//         state.language = "en";
+//       }
+//       window.location.reload();
+   
+//     }
+//   },
+// });
+
+// export const { setLanguage } = languageSlice.actions;
+// export default languageSlice.reducer;
